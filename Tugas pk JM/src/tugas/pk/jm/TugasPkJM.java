fae8971ad3eb2b803fb7400abaa3fbb84c2f4a35
pahrizal_(drew) <55535746+1-pahrizal@users.noreply.github.com>
2020-06-03 23:48:04 -1200

    Add files via upload

---
+++ Tugas pk JM/src/tugas/pk/jm/TugasPkJM.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.pk.jm;
+
+/**
+ *
+ * @author Arianarizal
+ */
+public class TugasPkJM {
+
+public static void main(String[] args){
+    int n = 0;
+    try{
+        n = System.in.read();
+    }
+    catch (java.io.IOException ex) {
+        System.err.println(ex.getMessage());
+        
+    }
+    System.out.println("Hasil :"+(char)n);
+    }
+    
+}
